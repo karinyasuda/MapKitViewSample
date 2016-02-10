65bc7e0ff37397d0186f786d5bfc8f34a139bdf9
Karin <karin.03k35@gmail.com>
2016-02-10 11:53:34 +0900

    Initial Commit

---
+++ MapKitViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapKitViewSample
+//
+//  Created by Karin on 2016/02/10.
+//  Copyright © 2016年 Karin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
